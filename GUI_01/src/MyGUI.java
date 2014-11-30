f7bce60b6de1a14f6943f77a5cae237914381b0a
AlanCarey <AlanC-ard-player@hotmail.com>
2014-11-30 20:36:39 +0000

    Changes to GUI & New pencil drawing
    New variable names.
    text Area will grow with frame when dragged .

--- GUI_01/src/MyGUI.java
+++ GUI_01/src/MyGUI.java
@@ -15,41 +15,45 @@ public class MyGUI {
 	
 	public static void main(String[] args){
 		JFrame frame = new JFrame();
-		frame.setSize(800,500);
+		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createTitledBorder("Text Editor"));
 		frame.add(panel);
 		
-		panel.setLayout(new MigLayout("debug", "[]10[]10[]","[][][]"));
+		panel.setLayout(new MigLayout("debug"));
 		
 		JLabel firstnameLabel = new JLabel("First Name");
 		JLabel familynameLabel = new JLabel("Family Name");
 		JTextField firstName = new JTextField(20);
 		JTextField lastName = new JTextField(20);
 		
-		JButton lab1 = new JButton("Label #1");	
-		JButton lab2 = new JButton("Label #2");
-		JButton lab3 = new JButton("Label #3");			
-		JButton lab4 = new JButton("Label #4");
-		JButton lab5 = new JButton("Label #5");			
-		JButton lab6 = new JButton("Label #6");
-		JButton lab7 = new JButton("Label #7");
+		JButton buttGetFile = new JButton("Get File");	
+		JButton buttReverse = new JButton("Revese");
+		JButton buttSave = new JButton("Save");			
+		JButton buttClearText = new JButton("Clear Text");
+		JButton buttEditText = new JButton("Edit Text");			
+		JButton buttSearch = new JButton("Search");
+		JButton buttSelectAll = new JButton("Select All");
+		JButton buttUpperCase = new JButton("Upper Case");	
+		JButton buttPrint = new JButton("Print");
+		JButton buttWordCount = new JButton("Word Count");			
+		JButton buttDeleteFile = new JButton("Delete File");
+		JButton buttSomthing = new JButton("Somthing");			
+		JButton buttCopyFile = new JButton("Copy File");
+		JButton buttSomthingElse = new JButton("Somthing Else");
 		
 		
 		JTextArea displayText = new JTextArea(30,30);
 		
-		panel.add(lab1,"wrap");
-		panel.add(lab2);
-		panel.add(displayText);
 		
-	
-		panel.add(lab3 ,"wrap");
-		panel.add(lab4, "wrap");
-		panel.add(lab5);
-		panel.add(lab6);
-		panel.add(lab7);
+		panel.add(buttGetFile, "span, center, wrap");
+		panel.add(buttSave, "split 2");
+		panel.add(displayText, "span2, push, grow"); //textArea will grow when dragged
+		panel.add(buttSearch, "wrap");
 
+		//trying to add more buttons under neath each other but having problems
+		//Also new pencil drawing in git repo
 	    
 		
 		frame.setVisible(true);
