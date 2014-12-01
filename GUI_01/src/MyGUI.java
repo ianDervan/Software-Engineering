3a4d16cff9f95eacb705945279f600a09e413d6c
AlanCarey <AlanC-ard-player@hotmail.com>
2014-12-01 18:55:42 +0000

    GUI nearly 78% done
    Added new GUI which grows with frame size. Need to add radio buttons and
    other Muck JOB! k

--- GUI_01/src/MyGUI.java
+++ GUI_01/src/MyGUI.java
@@ -15,21 +15,27 @@ public class MyGUI {
 	
 	public static void main(String[] args){
 		JFrame frame = new JFrame();
-		frame.setSize(500,500);
+		frame.setSize(750,700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		
 		JPanel panelButtons1 = new JPanel();
 		JPanel panelButtons2 = new JPanel();
+		JPanel panelTextField = new JPanel();
 		
-		panelButtons1.setBorder(BorderFactory.createTitledBorder("Buttons1"));
-		panelButtons1.setBorder(BorderFactory.createTitledBorder("Buttons2"));
+		panelButtons1.setBorder(BorderFactory.createTitledBorder("Main Buttons"));
+		panelButtons2.setBorder(BorderFactory.createTitledBorder("Secondary Buttons"));
+		panelTextField.setBorder(BorderFactory.createTitledBorder("TextFields"));
+		
+		frame.getContentPane().setLayout(new MigLayout());
+		frame.getContentPane().add(panelButtons1, "span, push, grow");
+		frame.getContentPane().add(panelTextField, "span, push, grow");
+		frame.getContentPane().add(panelButtons2, "span, push, grow");
 		
 		
-		frame.add(panelButtons1);
-		frame.add(panelButtons2);
-		
-		panelButtons1.setLayout(new MigLayout("debug"));
-		panelButtons2.setLayout(new MigLayout("debug"));
+		panelButtons1.setLayout(new MigLayout());
+		panelButtons2.setLayout(new MigLayout());
+		panelTextField.setLayout(new MigLayout());
 		
 		JButton buttGetFile = new JButton("Get File");	
 		JButton buttReverse = new JButton("Revese");
@@ -47,30 +53,31 @@ public class MyGUI {
 		JButton buttSomthingElse = new JButton("Somthing Else");
 		
 		
-		JTextArea displayText = new JTextArea(30,30);
-		
+		JTextArea displayTextMain = new JTextArea(30,30);
+		JTextArea displayTextSecondary = new JTextArea(15,30);
 		//trying to add panels of buttons as a separate panel so we can have 2 
 		//panels of separate buttons then insert another panel containing the text field.
 		
-		panelButtons1.add(buttGetFile, "span, grow, center, wrap");
-		panelButtons1.add(buttSave, "span, grow, center, wrap");
-		//panel.add(displayText, "span2, push, grow"); //textArea will grow when dragged
-		panelButtons1.add(buttSearch, "span, grow, center, wrap");
-		panelButtons1.add(buttSomthingElse, "span, grow, center, wrap");
-		panelButtons1.add(buttDeleteFile, "span, grow, center, wrap");
-		panelButtons1.add(buttSelectAll, "span, grow, center, wrap");
-		panelButtons1.add(buttPrint, "span, grow, center, wrap"); 
+		panelButtons1.add(buttGetFile);
+		panelButtons1.add(buttReverse);
+		panelButtons1.add(buttSave);
+		panelButtons1.add(buttClearText);
+		panelButtons1.add(buttEditText);
+		panelButtons1.add(buttSearch);
+		panelButtons1.add(buttSelectAll); 
 		
 		//panel containing text area inserted between the two panels of buttons
 		
-		panelButtons2.add(buttGetFile, "span, grow, center, wrap");
-		panelButtons2.add(buttSave, "span, grow, center, wrap");
-		//panel.add(displayText, "span2, push, grow"); //textArea will grow when dragged
-		panelButtons2.add(buttSomthing, "span, grow, center, wrap");
-		panelButtons2.add(buttUpperCase, "span, grow, center, wrap");
-		panelButtons2.add(buttCopyFile, "span, grow, center, wrap");
-		panelButtons2.add(buttClearText, "span, grow, center, wrap");
-		panelButtons2.add(buttWordCount, "span, grow, center, wrap");
+		panelTextField.add(displayTextMain, "push, grow"); //textArea will grow when dragged
+		panelTextField.add(displayTextSecondary, "push, grow"); //textArea will grow when dragged
+		
+		panelButtons2.add(buttUpperCase);
+		panelButtons2.add(buttPrint);
+		panelButtons2.add(buttWordCount);
+		panelButtons2.add(buttDeleteFile);
+		panelButtons2.add(buttSomthing);
+		panelButtons2.add(buttCopyFile);
+		panelButtons2.add(buttSomthingElse);
 	    
 		
 		frame.setVisible(true);
